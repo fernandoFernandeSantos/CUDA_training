9001a9bd44c25f65f7d541032ff736a49a5a9fda
reliabilitylogdata <reliabilitylogdata@github.com>
2016-11-13 18:59:21 -0200

    mat

--- test_mm/src/mat_mul.cu
+++ test_mm/src/mat_mul.cu
@@ -303,14 +303,17 @@ void matrix_multiplication_abft() {
 	printf("compare matrices\n");
 	//compare(host_array_c, host_array_c_temp, VECTOR_SIZE_C);
 	cudaFree(device_array_a);
-	printf("passou a\n");
 	cudaFree(device_array_b);
-	printf("passou array b\n");
 	cudaFree(device_array_c);
-	printf("problem with host arrays\n");
 	free(host_array_a);
+	printf("problem with host arrays a\n");
+
 	free(host_array_b);
+	printf("problem with host arrays b\n");
+
 	free(host_array_c);
+	printf("problem with host arrays c\n");
+
 	free(host_array_c_temp);
 }
 
