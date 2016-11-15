9402876858114f092d93616cfbc97e4acf7e18f9
reliabilitylogdata <reliabilitylogdata@github.com>
2016-11-15 13:21:23 -0200

    mat

--- test_mm/src/mat_mul.cu
+++ test_mm/src/mat_mul.cu
@@ -84,7 +84,7 @@ __global__ void first_abraham_op(double *a, long rows_a, long cols_a) {
 		acc += a[k * (cols_a - 1) + i];
 	}
 	printf("a_index %ld acc %lf \n", rows_a * cols_a + i, acc);
-	long a_index = rows_a * cols_a + i;
+	long a_index = (rows_a - 1) * cols_a + i;
 	a[a_index] = acc;
 }
 
