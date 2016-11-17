3f753d2e2f4a4d2a77f65a0c1e170994ced565ba
reliabilitylogdata <reliabilitylogdata@github.com>
2016-11-17 02:57:08 -0200

    fixing abft

--- test_mm/src/mat_mul.cu
+++ test_mm/src/mat_mul.cu
@@ -227,7 +227,7 @@ void fill_mat_row_major(float *t, long m, long n) {
 	long i, j;
 	for (i = 0; i < m; i++)
 		for (j = 0; j < n; j++)
-			t[i * n + j] = (i * j) /(rand()  % 10);
+			t[i * n + j] = (i * j) /(rand()  % 10 + 1);
 }
 
 void fill_mat_collum_major(float *t, long m, long n) {
