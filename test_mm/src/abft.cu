a7416d0ef0b5ef943e7ec811e5d69e1d4bf865a9
reliabilitylogdata <reliabilitylogdata@github.com>
2017-02-27 12:48:18 -0300

    testing

--- test_mm/src/abft.cu
+++ test_mm/src/abft.cu
@@ -615,7 +615,7 @@ cublasStatus_t dgemv_host(int width_a, int height_a, int width_b,
 	int lda = width_a;
 
 	cublasStatus_t ret = cublasSgemv(handle, CUBLAS_OP_T, height_a, width_a, &alpha, a, lda, b, 1, &beta, c, 1);
-
+	printf("passou\n");
 	if (CUBLAS_STATUS_SUCCESS != ret) {
 		printf("pau no blas\n");
 		exit(-1);
