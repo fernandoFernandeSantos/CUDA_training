a7f9d2db64aa030d3e070e66bbe653a482548eb7
reliabilitylogdata <reliabilitylogdata@github.com>
2017-02-27 17:57:29 -0300

    testing

--- test_mm/src/abft.cu
+++ test_mm/src/abft.cu
@@ -484,7 +484,7 @@ void calc_checksums_from_host(float *a, float *b, long_t rows_a, long_t cols_a,
 
 	calc_collum_checksum<<<blocks, threads_blocks>>>(a, rows_a, cols_a);
 
-	printf("first blocks %ld threads %ld %ld\n", blocks, threads_col, threads_row);
+	printf("first blocks %ld threads %ld %ld\n", blocks, threads_blocks.x, threads_blocks.y);
 	//second
 	blocks = ceil(float(rows_b) / float(BLOCK_SIZE));
 	threads_col = ceil(float(cols_b) / float(blocks));
