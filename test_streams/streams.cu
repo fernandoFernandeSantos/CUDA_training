a08aeabdc197f5b421ff5e66f5acfb9bfbb348c5
Fernando <f_157@hotmail.com>
2018-02-15 13:51:46 -0200

    streams

--- test_streams/streams.cu
+++ test_streams/streams.cu
@@ -25,13 +25,39 @@ void *launch_kernel(void *dummy) {
 	return NULL;
 }
 
+
+///////////////////////////////////////////
+//Matrix multiplication pthreads
+
+typedef struct{
+	float *a_device;
+	float *b_device;
+	float *c_device;
+	int a_col_size;
+	int a_lin_size;
+
+	int b_col_size;
+	int b_lin_size;
+
+}thread_parameters;
+
+
+void *launch_sgemm(void *data){
+	thread_parameters *parameter = (thread_parameters*)data;
+
+	printf("Thread Id: %d a_col %d a_lin %d b_col %d b_lin %d\n", pthread_self(), parameter->a_col_size, parameter->a_lin_size,
+			parameter->b_col_size, parameter->b_lin_size);
+
+	return NULL;
+}
+
 int main() {
 	const int num_threads = 8;
 
 	pthread_t threads[num_threads];
 
 	for (int i = 0; i < num_threads; i++) {
-		if (pthread_create(&threads[i], NULL, launch_kernel, 0)) {
+		if (pthread_create(&threads[i], NULL, launch_sgemm, 0)) {
 			fprintf(stderr, "Error creating threadn");
 			return 1;
 		}
