158eda45b34a963adbee5f9a52425327ff1fa080
reliabilitylogdata <reliabilitylogdata@github.com>
2018-02-14 23:22:29 -0200

    streams

--- test_streams/streams.cu
+++ test_streams/streams.cu
@@ -6,7 +6,11 @@ const int N = 1 << 20;
 __global__ void kernel(float *x, int n) {
 	int tid = threadIdx.x + blockIdx.x * blockDim.x;
 	for (int i = tid; i < n; i += blockDim.x * gridDim.x) {
-		x[i] = sqrt(pow(3.14159, i));
+		double sum = 0;
+		for (int j = 100; j < N; j++){
+			sum += sqrt(pow(3.14159, i))/j;
+		}
+		x[i] = sum;
 	}
 }
 
