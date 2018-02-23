2d767ed9fd6a27d5656e1cfb2460487d0d3cd28e
reliabilitylogdata <reliabilitylogdata@github.com>
2018-02-23 11:07:42 -0300

    streams

--- test_streams/streams.cu
+++ test_streams/streams.cu
@@ -61,7 +61,11 @@ inline void __cudaCheckError(const char *file, const int line,
 __global__ void kernel(float *x, int n) {
 	int tid = threadIdx.x + blockIdx.x * blockDim.x;
 	for (int i = tid; i < n; i += blockDim.x * gridDim.x) {
-		x[i] = sqrt(pow(3.14159, i));
+		double sum = 0;
+		for(int j = 0; j < 1000; j++){
+			sum += sqrt(pow(3.14159, i)) / float(j);
+		}
+		x[i] = sum;
 	}
 }
 
