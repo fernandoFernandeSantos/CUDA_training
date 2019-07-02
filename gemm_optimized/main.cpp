1eb59fd7937e14a12deaf044c2c0e54e098d3963
Your Name <fernandofernandesantos@gmail.com>
2019-07-01 20:43:07 -0600

    main

--- gemm_optimized/main.cpp
+++ gemm_optimized/main.cpp
@@ -17,6 +17,21 @@
 typedef double real_t;
 typedef float half_real_t;
 
+void gemm_host(std::vector<real_t>& a, std::vector<real_t>& b,
+		std::vector<real_t>& c, real_t alpha, real_t beta, int m, int n,
+		int k) {
+
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			real_t sum = 0;
+			for (int p = 0; p < k; p++) {
+				sum += a[i * m + k] * b[k * n + j];
+			}
+			c[i * m + j] = alpha * sum + beta * c[i * m + j];
+		}
+	}
+}
+
 int main(int argc, char **argv) {
 
 	int m;
@@ -33,44 +48,54 @@ int main(int argc, char **argv) {
 	std::vector<real_t> host_a(m * n, alpha);
 	std::vector<real_t> host_b(n * k, beta);
 	std::vector<real_t> host_c(m * k, 0.0);
-	std::vector<half_real_t> host_c_inc(m*k, 0.0);
+	std::vector<half_real_t> host_c_inc(m * k, 0.0);
 
-	rad::DeviceVector<real_t> device_c(host_c), device_a(host_a), device_b(host_b);
+	rad::DeviceVector<real_t> device_c(host_c), device_a(host_a), device_b(
+			host_b);
 	rad::DeviceVector<half_real_t> device_c_inc(host_c_inc);
 
 	cudaStream_t st;
 	cudaStreamCreate(&st);
 	assert(m > 512 && n > 512 && m % 64 == 0 && n % 16 == 0 && k % 16 == 0);
 
-	for(int t = 0; t < 10; t++){
+	for (int t = 0; t < 10; t++) {
 		device_c = zero_vector;
 		device_c_inc = zero_vector_inc;
 		sgemm(st, device_c.data(), device_a.data(), device_b.data(), m, n, k,
-					lda, ldb, ldc, alpha, beta);
+				lda, ldb, ldc, alpha, beta);
 		device_c = zero_vector;
 		device_c_inc = zero_vector_inc;
-		sgemm_dmr(st, device_c.data(), device_c_inc.data(), device_a.data(), device_b.data(), m, n, k,
-					lda, ldb, ldc, alpha, beta);
+		sgemm_dmr(st, device_c.data(), device_c_inc.data(), device_a.data(),
+				device_b.data(), m, n, k, lda, ldb, ldc, alpha, beta);
 
 	}
 
 	host_c = device_c.to_vector();
 	host_c_inc = device_c_inc.to_vector();
 
-	for(int i = 0; i < 10; i++){
-		for(int j = 0; j < 10; j++){
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
 			std::cout << host_c[i * m + j] << " ";
 		}
 		std::cout << std::endl;
 	}
 	std::cout << "Incomplete type" << std::endl;
-	for(int i = 0; i < 10; i++){
-		for(int j = 0; j < 10; j++){
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
 			std::cout << host_c_inc[i * m + j] << " ";
 		}
 		std::cout << std::endl;
 	}
 
+	std::vector<real_t> test_gemm(m * k, 0);
+	gemm_host(host_a, host_b, test_gemm, alpha, beta, m, n, k);
+
+	for (int i = 0; i < test_gemm.size(); i++) {
+		auto g = test_gemm[i], f = host_c[i];
+		if (g != f) {
+			std::cout << "HOST " << g << " GPU " << f << std::endl;
+		}
+	}
 	cudaStreamDestroy(st);
 	return 0;
 }
