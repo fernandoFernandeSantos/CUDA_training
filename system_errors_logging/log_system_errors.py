fbfbdd557ccae0ea807bf2d4afa93b8010a8b258
Fernando Fernandes <fernandofernandesantos@gmail.com>
2021-05-25 20:59:55 -0300

    update on log system errors

--- system_errors_logging/log_system_errors.py
+++ system_errors_logging/log_system_errors.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+import configparser
 import os
 import time
 from datetime import datetime
@@ -10,6 +10,8 @@ SLEEP = 1
 # Max GB allowed
 MAX_MB_ALLOWED = 2048
 SEARCH_NVIDIA_STRINGS = {"nvidia", "nvrm"}
+# In seconds
+PRINTING_SECONDS_INTERVAL = 120
 
 
 def grep_for_nvidia_errors(file, offset):
@@ -26,29 +28,47 @@ def grep_for_nvidia_errors(file, offset):
     return ret, offset
 
 
+def get_radiation_benchmarks_log_dir():
+    try:
+        config = configparser.RawConfigParser()
+        config.read(CONF_FILE)
+
+        var_dir = config.get('DEFAULT', 'vardir') + "/"
+        return var_dir
+    except IOError as e:
+        raise IOError("System configuration setup error: " + str(e))
+
+
 def main():
     global SLEEP
     var_dir_logs = "/var/log"
+    rad_var_dir = get_radiation_benchmarks_log_dir()
     # The file and its offset
     system_log_files = {
         "syslog": 0, "kern.log": 0
     }
-    out_file = "/var/radiation-benchmarks/log/sys_logs.log"
+
+    sys_log_output_file_name = datetime.fromtimestamp(int(time.time())).strftime("%Y_%m_%d_%H_%M_%S")
+    sys_log_output_file_name = f"{rad_var_dir}/log/{sys_log_output_file_name}_sys_logs.log"
 
     # The thread will work until ctrl-c is pressed
+    last_print = 0
     while True:
         for log_file in system_log_files:
             sys_log_file = f"{var_dir_logs}/{log_file}"
             searched_list, system_log_files[log_file] = grep_for_nvidia_errors(
                 sys_log_file, system_log_files[log_file])
-            with open(out_file, "a") as out_fp:
+            with open(sys_log_output_file_name, "a") as out_fp:
                 out_fp.writelines(searched_list)
         # Convert to MB
-        file_size = os.path.getsize(out_file) / 1024 ** 2
-        print(f"Updating the system log size {file_size:.3}MB at timestamp {datetime.fromtimestamp(int(time.time()))}")
+        file_size = os.path.getsize(sys_log_output_file_name) / 1024 ** 2
         if file_size > MAX_MB_ALLOWED:
             break
 
+        if last_print == 0:
+            print(f"Updating the system log file {sys_log_output_file_name}"
+                  f" size {file_size:.3}MB at timestamp {datetime.fromtimestamp(int(time.time()))}")
+        last_print = (last_print + 1) % (PRINTING_SECONDS_INTERVAL / SLEEP)
         time.sleep(SLEEP)
 
 
